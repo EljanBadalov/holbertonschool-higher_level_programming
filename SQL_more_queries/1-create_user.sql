d281f9accd6fa5299010dd887820d10b99117f6d
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-05-13 00:07:18 +0400

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,4 @@
--- Create the user if they do not already exist and set the password
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges on all databases and tables to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Reload the privileges from the grant tables in the MySQL system database
-FLUSH PRIVILEGES;
+-- Set privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
