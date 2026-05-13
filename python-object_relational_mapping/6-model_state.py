e5e8f9e6e8090abca0b019e3b5b24e88ab432abe
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-05-13 22:13:36 +0400

    Update 6-model_state.py

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
-import sys
-from model_state import Base, State
+"""Creates the states table."""
 
-from sqlalchemy import (create_engine)
+import sys
+from sqlalchemy import create_engine
+from model_state import Base
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
     Base.metadata.create_all(engine)
