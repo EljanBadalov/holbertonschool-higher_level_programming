25402b5fcef145577a5253a8a5f580717758e05c
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-05-13 19:50:48 +0400

    Add files via upload

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+State model
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from base import Base
+
+
+class State(Base):
+    """
+    class
+    """
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", back_populates="state", cascade="all, delete")
