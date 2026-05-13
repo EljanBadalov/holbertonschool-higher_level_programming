079e0809edb0ad6e63f6a21db543d2d561582210
EljanBadalov <elcanbadalov06@gmail.com>
2026-05-13 17:16:53 +0000

    Fix task 0 select states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa
+This script lists all states from the given MySQL database.
 """
 
-import MySQLdb
 import sys
+import MySQLdb
 
 
 if __name__ == "__main__":
@@ -13,11 +13,12 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf8"
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     rows = cur.fetchall()
 
@@ -26,3 +27,4 @@ if __name__ == "__main__":
 
     cur.close()
     db.close()
+
