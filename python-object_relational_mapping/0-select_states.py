d92c15e1a72f25c8f0811f99c3d6cd2a1e565d4a
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-05-13 21:04:20 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
-"""Python - Object-relational mapping - task 0. Get all states"""
+"""Lists all states from the database hbtn_0e_0_usa."""
 
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
+import sys
+import MySQLdb
 
-    if len(sys.argv) != 4:
-        sys.exit('Use: 0-select_states.py <mysql username> <mysql password>'
-                 ' <database name>')
 
-    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+    cursor.execute(query)
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
     
