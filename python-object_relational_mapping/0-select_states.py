a102bfe90a9e41b14bac9813effa483c89fbc52d
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-05-13 19:53:23 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""
-this is global enviroment
-"""
-
-
-import sys
+"""Lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    """
-        this is local enviroment
-    """
+    # Get arguments from command line
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -23,15 +19,20 @@ if __name__ == "__main__":
         db=database
     )
 
+    # Create cursor object
     cursor = db.cursor()
 
+    # Execute SQL query
     query = "SELECT * FROM states ORDER BY id ASC;"
     cursor.execute(query)
 
+    # Fetch all results
     states = cursor.fetchall()
 
+    # Print results in required format
     for state in states:
         print(state)
 
+    # Close cursor and database connection
     cursor.close()
     db.close()
