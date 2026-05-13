4d325fdded3b2c1b8515123b7befb8313e20abf4
EljanBadalov <elcanbadalov06@gmail.com>
2026-05-13 18:38:18 +0000

    Add City model and fetch cities by state

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-"""
-14-model_city_fetch_by_state.py
-"""
+"""This script lists all City objects by State."""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from model_state import Base, State
 from model_city import City
 
 
 if __name__ == "__main__":
-    usnm = sys.argv[1]
-    pwd = sys.argv[2]
-    db = sys.argv[3]
-
     engine = create_engine(
-        f"mysql+mysqldb://{usnm}:{pwd}@localhost:3306/{db}"
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_cities = (session.query(State, City).
-                     join(City, State.id == City.state_id).
-                     order_by(City.id).all())
+    result = session.query(State, City).filter(
+        State.id == City.state_id
+    ).order_by(City.id).all()
 
-    for state, city in states_cities:
-        print('{}: ({}) {}'.format(state.name, city.id, city.name))
+    for state, city in result:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
+
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-City model
-"""
+"""This module defines the City class and its database mapping."""
 
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from base import Base
+from sqlalchemy import Column, ForeignKey, Integer, String
+from model_state import Base
 
 
 class City(Base):
-    """
-    citty
-    """
+    """This class represents a city in the cities table."""
+
     __tablename__ = "cities"
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True)
     name = Column(String(128), nullable=False)
-
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
 
-    state = relationship("State", back_populates="cities")
