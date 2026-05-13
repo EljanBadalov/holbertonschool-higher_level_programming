440813cf121e1313b844ce4754e7175f2720a07d
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2026-05-13 19:58:32 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Lists all states from the database hbtn_0e_0_usa."""
 
-import sys
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
@@ -15,7 +15,9 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
+
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
     rows = cur.fetchall()
 
     for row in rows:
